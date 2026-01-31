918e54364defa06b10a6955b132439dcf311f7d7
indenterror20 <gaac.karlalden@ncba.edu.ph>
2026-01-31 11:44:21 +0800

    starting to write python

--- pyhtons/orders.py
+++ pyhtons/orders.py
@@ -1,5 +1,17 @@
 # fetch api 
 
 #do the process
+class Orders:
+    def__init__(Orders, OrderId, OrderDate, CustomerName, OrderType, TotalItems, OrderTotal, PaymentStatus, OrderStatus)
+    Orders.OrderId = OrderId
+    Orders.OrderDate = OrderDate
+    Orders.CustomerName = CustomerName
+    Orders.OrderType = OrderType
+    Orders.TotalItems = TotalItems
+    Orders.OrderTotal = OrderTotal
+    Orders.PaymentStatus = PaymentStatus
+    Orders.OrderStatus = OrderStatus
+
+    
 
 #send it into javascript (visual datas)
